68aad82ca80c38f706f0c06ba980e96dda77d5b9
hashim715 <mharus575@gmail.com>
2021-11-29 09:42:16 -0800

    solved error

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const http = require("http");
 const fs = require('fs');
 const homefile=fs.readFileSync("index.html","utf-8");
+const port = process.env.PORT || 8000;
 const server=http.createServer((req,res)=>{
     if(req.url=='/'){
         res.write(homefile);
         res.end();
     }
 });
-server.listen(8000,'127.0.0.1',()=>{
+server.listen(port,'127.0.0.1',()=>{
     console.log("listeining to the server");
 });
